306631137e5c7645076e68e1e92ca4b3d95d5973
nameShuffle <gudanova-v@mail.ru>
2018-07-03 10:49:50 +0300

    Empty editor page

--- web-side/strl/STRLite/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StrliteConfig(AppConfig):
-    name = 'STRLite'
--- web-side/strl/strl/settings.py
+++ web-side/strl/strl/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'strlite.apps.StrliteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'strl.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'strl_db',
+        'USER': 'strl_admin',
+        'PASSWORD': '521skb',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
