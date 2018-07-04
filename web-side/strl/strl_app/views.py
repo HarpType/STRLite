ceb21c2dd27417415f0c4f06edd103c781e62e26
nameShuffle <gudanova-v@mail.ru>
2018-07-04 12:46:10 +0300

    Revert "pizdec"
    This reverts commit 4793e369d96c93f604ef7fc58ed819f44aa42f8c.

--- web-side/strl/strl_app/views.py
+++ web-side/strl/strl_app/views.py
@@ -2,8 +2,6 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-import json
-import time
 
 import json
 
@@ -49,13 +47,6 @@ def start(request):
             files.data2[0] = 'R'
             return HttpResponse(json.dumps({'st': 'ready'}))
 
-def properties(request):
-	t = time.time() % 20 * 3
-	data = [{'x': 100 + 10 * t, 'y': 100, 'a': 0.0, 'r':25},
-			{'x': 100 + 20 * (t%30), 'y': 200, 'a': 0.0, 'r':20},
-			{'x': 100 + 30 * (t%20), 'y': 300, 'a': 0.0, 'r':15},
-			{'x': 100 + 40 * (t%15), 'y': 400, 'a': 0.0, 'r':10}]
-	return HttpResponse(json.dumps(data))
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
