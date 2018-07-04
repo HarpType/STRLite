51c03111ad4c9c018d3a3faa74fbe859476c38a0
nameShuffle <gudanova-v@mail.ru>
2018-07-04 12:18:46 +0300

    Ready to add gravity

--- web-side/strl/strl_app/views.py
+++ web-side/strl/strl_app/views.py
@@ -2,19 +2,50 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+
 import json
 
+import subprocess
+import mmap
+import pickle
 
-def home(request):
-    return HttpResponse("Hello!")
+from strl_app.relation import files
 
 
-def editor(request):
-    return HttpResponse("Editor!!!!")
+# home view is just from template in urls
+# editor view is just from template in urls
+
+def stop(request):
+    files.data2[0] = 'S'
+    code = files.proc.wait()
+    files.data1.close()
+    files.data2.close()
+    files.f1.close()
+    files.f2.close()
 
 
 def properties(request):
-    return HttpResponse(json.dumps({'x': 100, 'y': 100}))
+    # size = int(files.data1[:4])
+    # str_data = files.data1 [4:4+size]
+    data_to_send = pickle.loads(files.data1)
+    return HttpResponse(json.dumps(data_to_send))
+
+
+def start(request):
+    files.f1 = open('relation/c_to_p.dat', 'r+')
+    files.f2 = open('relation/p_to_c.dat', 'r+')
+
+    size = 1024
+
+    files.data1 = mmap.mmap(files.f1.fileno(), size)
+    files.data2 = mmap.mmap(files.f2.fileno(), size)
+
+    files.proc = subprocess.Popen(['python3', files.child_path])
+
+    while True:
+        if files.data1[0] == 'R':
+            files.data2[0] = 'R'
+            return HttpResponse(json.dumps({'st': 'ready'}))
 
 
 class SignUp(generic.CreateView):
