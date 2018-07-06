22f0bc18f199dea9c5a00e1463cd9301b497a181
Koekin Yaroslav <ykoekin@gmail.com>
2018-07-06 11:14:23 +0300

    adding the ros-side folder

---
+++ ros-side/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gmock
+# Build directory: /home/yaroslav/programming/STRLite/ros-side/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
