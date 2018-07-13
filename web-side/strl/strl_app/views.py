e9cfba7b5c643b9be67745de155bb9dd48d9c173
nameShuffle <gudanova-v@mail.ru>
2018-07-13 10:18:39 +0300

    Add delete button un worlds

--- web-side/strl/strl_app/views.py
+++ web-side/strl/strl_app/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from strl_app.models import World
 
@@ -35,32 +35,46 @@ def editor(request):
         return HttpResponseRedirect('/login/?next=%s' % request.path)
 
 
-def create(request):
+def create_world(request):
     if request.user.is_authenticated:
-        if request.method == 'POST':
+        # if request.method == 'POST':
             # принимаю от Паши данные
             # init_json = request.POST.data
             init_json = {'some': 'thing'}
             w = World(owner=request.user, init_info=init_json)
             w.save()
             return HttpResponse(str(w.id))
-        else:
-            return HttpResponse("Nothing to create")
+        #else:
+            #return HttpResponse("Nothing to create")
     else:
         return HttpResponseRedirect('/login/')
 
 
-def projects(request):
+def worlds(request):
     if request.user.is_authenticated:
         worlds_qs = World.objects.filter(owner=request.user)
-        id_dicts = worlds_qs.values('id')
-        # id_list[i]['id'] - получение нужного id
-        id_list = []
-        for i in id_dicts:
-            id_list.append(i['id'])
-        print(id_list)
-        context = {'id_list': id_list}
+        context = {'world_list': worlds_qs}
         return render(request, 'worlds.html', context)
+    else:
+        return HttpResponseRedirect('/login/')
+
+
+def delete_world(request, world_id):
+    if request.user.is_authenticated:
+        world = get_object_or_404(World, pk=world_id, owner=request.user)
+        world.delete()
+        return HttpResponseRedirect('/worlds/')
+    else:
+        return HttpResponseRedirect('/login/')
+
+
+def player(request, world_id):
+    if request.user.is_authenticated:
+        world = get_object_or_404(World, pk=world_id, owner=request.user)
+        json = world.init_info
+        return HttpResponse(json)
+        # context = {'world_id': world_id}
+        # return render(request, 'player.html', context)
     else:
         return HttpResponseRedirect('/login/')
 
