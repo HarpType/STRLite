752de9b44e888e22e7c7a301adaacf1a7423dc21
nameShuffle <gudanova-v@mail.ru>
2018-07-16 14:42:22 +0300

    Change editor logic

--- ros-side/build/gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/yaroslav/programming/STRLite/ros-side/build/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
